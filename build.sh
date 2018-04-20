350a060de11aacf45f4b3d4752d8fb632e94cf6e
Ilya Ostrovskiy <iostat@users.noreply.github.com>
2018-04-20 14:16:12 -0400

    build by VERSION then tag as latest, and make default geth version 1.8.4

--- build.sh
+++ build.sh
@@ -3,10 +3,10 @@ set -e
 set -x 
 
 export IMAGE_NAME=${IMAGE_NAME:-"foamspace/cliquebait"}
-export GETH_VERSION=${GETH_VERSION:-"v1.7.3"}
+export GETH_VERSION=${GETH_VERSION:-"v1.8.4"}
 
-docker build --pull --build-arg GETH_VERSION=$GETH_VERSION -t $IMAGE_NAME:latest .
-docker tag $IMAGE_NAME:latest $IMAGE_NAME:$GETH_VERSION
+docker build --pull --build-arg GETH_VERSION=$GETH_VERSION -t $IMAGE_NAME:$GETH_VERSION .
+docker tag $IMAGE_NAME:$GETH_VERSION $IMAGE_NAME:latest
 
 # docker push $IMAGE_NAME:latest
 # docker push $IMAGE_NAME:$GETH_VERSION
